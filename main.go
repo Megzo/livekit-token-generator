b8e76b1f3bc24b33a9efcbb68e2da65e7651ebb8
Péter Megyesi <megyesi@tmit.bme.hu>
2023-06-12 15:32:25 +0200

    return with raw json token

--- main.go
+++ main.go
@@ -2,14 +2,46 @@ package main
 
 import (
     "fmt"
+	"encoding/base64"
+	"encoding/json"
 	"log"
 	"os"
     "net/http"
+	"strings"
 	"time"
 
 	//lksdk "github.com/livekit/server-sdk-go"
 	"github.com/livekit/protocol/auth"
 )
+
+// Decodes a JWT token into a JSON string
+func DecodeJWT(token string) (string, error) {
+	// Split the token into its three parts: header, payload, and signature
+	parts := strings.Split(token, ".")
+	if len(parts) != 3 {
+		return "", fmt.Errorf("invalid JWT token format")
+	}
+
+	// Decode and parse the payload part
+	payloadBytes, err := base64.RawURLEncoding.DecodeString(parts[1])
+	if err != nil {
+		return "", fmt.Errorf("failed to decode JWT payload: %v", err)
+	}
+
+	var payloadMap map[string]interface{}
+	err = json.Unmarshal(payloadBytes, &payloadMap)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse JWT payload: %v", err)
+	}
+
+	// Encode the payload as a JSON string
+	payloadJSON, err := json.MarshalIndent(payloadMap, "", "  ")
+	if err != nil {
+		return "", fmt.Errorf("failed to encode JWT payload as JSON: %v", err)
+	}
+
+	return string(payloadJSON), nil
+}
 
 func getJoinToken(apiKey, apiSecret, room, identity string) (string, error) {
     canPublish := true
@@ -26,7 +58,12 @@ func getJoinToken(apiKey, apiSecret, room, identity string) (string, error) {
 		SetIdentity(identity).
 		SetValidFor(time.Hour)
 
-	return at.ToJWT()
+	jwtToken, err := at.ToJWT()
+	if err != nil {
+		fmt.Printf("Error in token generation: %s", err)
+	}
+
+	return DecodeJWT(jwtToken)
 }
 
 
